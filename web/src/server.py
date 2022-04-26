7f6ad2d458a6c14c23d789aa5c97e301d2b6f507
Cozmo8 <97196858+Cozmo8@users.noreply.github.com>
2022-04-25 22:20:51 -0700

    Update Files

--- web/src/server.py
+++ web/src/server.py
@@ -13,6 +13,9 @@ def get_product(req):
 def get_proposition(req):
     return FileResponse("templates/proposition.html")
 
+def get_team(req):
+    return FileResponse("templates/team.html")
+
 if __name__ == '__main__':
   with Configurator() as config:
     config.add_route('home', '/')
@@ -23,6 +26,9 @@ if __name__ == '__main__':
 
     config.add_route('proposition', '/proposition')
     config.add_view(get_proposition, route_name='proposition')
+
+    config.add_route('team', '/team')
+    config.add_view(get_team, route_name='team')
 
     config.add_static_view(name='/', path='./public', cache_max_age=3600)
     app = config.make_wsgi_app()
